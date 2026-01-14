f682984c2040d53f1a1393c388f5a075367702bc
grown-tree <tjsruddks12@naver.com>
2026-01-14 14:56:21 +0900

    exit입력전까지 계산반복 exit입력시 종료 구현

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -9,56 +9,65 @@ public class Calculator {
         int num1,num2 = 0;//사용자입력 숫자저장 변수
         char cal= ' '; //연산기호저장 변수
         int result = 0; //결과저장 변수
+        String exit= ""; //종료 변수
 
         Scanner sc = new Scanner(System.in);//사용자로부터 입력받기위한 인스턴스
-
-        while(true){//무한반복
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            num1 = sc.nextInt();
-            if (num1 >=0){//입력받은 숫자가 0보다 큰 경우 반복문 탈출
-                break;
-            }else{//그외 모든경우
-                System.out.println("0보다 큰 숫자를 입력해주세요 ");
+        while (true) {
+            while (true) {//무한반복
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                num1 = sc.nextInt();
+                if (num1 >= 0) {//입력받은 숫자가 0보다 큰 경우 반복문 탈출
+                    break;
+                } else {//그외 모든경우
+                    System.out.println("0보다 큰 숫자를 입력해주세요 ");
+                }
             }
-        }
-        while(true) {
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            num2 = sc.nextInt();
-            if (num2 >=0) {
-                break;
-            } else {
-                System.out.println("0보다 큰 숫자를 입력해주세요 ");
+            while (true) {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                num2 = sc.nextInt();
+                if (num2 >= 0) {
+                    break;
+                } else {
+                    System.out.println("0보다 큰 숫자를 입력해주세요 ");
+                }
             }
+
+            System.out.println("사칙연산 기호를 입력해주세요 : +, -, *, /");
+            cal = sc.next().charAt(0);//입력값중 첫번째글자만 char형태 변수 cal에 저장
+
+            switch (cal) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+
+                    if (num2 == 0) System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("+, -, *, / 중 하나를 입력해주세요.");
+                    break;
+            }
+            System.out.println("결과 : " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            exit = sc.next();
+
+            if(exit.equals("exit")){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
         }
 
-        //System.out.print("첫번째 숫자: "+num1+"\n2번째 숫자: "+num2);//출력문으로 동작확인
-
-        System.out.println("사칙연산 기호를 입력해주세요 : +, -, *, /");
-        cal = sc.next().charAt(0);//입력값중 첫번째글자만 char형태 변수 cal에 저장
-
-        switch (cal) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-
-                if (num2 == 0)System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-
-                result = num1 / num2;
-                break;
-            default:
-                System.out.println("+, -, *, / 중 하나를 입력해주세요.");
-                break;
-        }
-        System.out.println("결과 : "+result);
-
-        }
+    }
 
 }
 
